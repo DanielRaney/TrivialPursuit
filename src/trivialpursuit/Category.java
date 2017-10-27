0b92de475448a3906fb625de1d635cee5914295d
natewise <nate3wise@gmail.com>
2017-10-27 10:40:54 -0700

    new Category class

---
+++ src/trivialpursuit/Category.java
@@ -0,0 +1,10 @@
+
+package trivialpursuit;
+
+public class Category {
+    public static final int NUM_CATEGORIES = 6;
+    public static int categories[] = new int[NUM_CATEGORIES];
+    public static enum Categories{
+        
+    }
+}
