8e45794d4ba10fed6fd7246f454d4da5d40b9c61
Daniel Raney-154003389 <154003389@vusdnoc.vusd.k12.ca.us>
2017-10-27 09:43:55 -0700

    A

---
+++ src/trivialpursuit/Board.java
@@ -0,0 +1,51 @@
+
+package trivialpursuit;
+
+import java.awt.*;
+
+public class Board {
+    
+    private final static int NUM_ROWS = 100;
+    private final static int NUM_COLUMNS = 100; 
+    
+    public static void Reset() {
+        
+    }
+    
+    public static void addPiece(int x, int y) {
+        int ydelta = Window.getHeight2()/NUM_ROWS;
+        int xdelta = Window.getWidth2()/NUM_COLUMNS;
+        
+        int zx = (x - Window.getX(0))/xdelta;
+        int zy = (y-Window.getY(0))/ydelta;
+    }
+    
+    
+    public static void Draw(Graphics2D g) {
+        int x[] = {Window.getX(0), Window.getX(Window.getWidth2()), Window.getX(Window.getWidth2()), Window.getX(0), Window.getX(0)};
+        int y[] = {Window.getY(0), Window.getY(0), Window.getY(Window.getHeight2()), Window.getY(Window.getHeight2()), Window.getY(0)};
+//fill border
+        g.setColor(Color.white);
+        g.fillPolygon(x, y, 4);
+// draw border
+        g.setColor(Color.red);
+        g.drawPolyline(x, y, 5);
+//Calculate the width and height of each board square.
+        int ydelta = Window.getHeight2()/NUM_ROWS;
+        int xdelta = Window.getWidth2()/NUM_COLUMNS;
+//draw grid
+        g.setColor(Color.black);
+        for (int zi = 1;zi<NUM_ROWS;zi++)
+        {
+            g.drawLine(Window.getX(0),Window.getY(zi*ydelta), Window.getX(Window.getWidth2()),Window.getY(zi*ydelta));
+        }
+        
+        for (int zi = 1;zi<NUM_COLUMNS;zi++)
+        {
+            g.drawLine(Window.getX(zi*xdelta),Window.getY(0),Window.getX(zi*xdelta),Window.getY(Window.getHeight2()));
+        }
+//Fill in below here
+        
+        
+    }
+}
---
+++ src/trivialpursuit/Window.java
@@ -0,0 +1,30 @@
+
+package trivialpursuit;
+public class Window {
+    public static int xsize = -1;
+    public static int ysize = -1;
+    
+    static final int XBORDER = 25;
+    static final int YBORDER = 25;
+    static final int YTITLE = 25;
+    static final int WINDOW_WIDTH = 1000;
+    static final int WINDOW_HEIGHT = 1000;    
+    public static int getX(int x) {
+        return (x + XBORDER);
+    }
+
+    public static int getY(int y) {
+        return (y + YBORDER + YTITLE);
+    }
+    public static int getYNormal(int y) {
+        return (-y + YBORDER + YTITLE+getHeight2());
+    }
+
+    public static int getWidth2() {
+        return (xsize - getX(0) - XBORDER);
+    }
+
+    public static int getHeight2() {
+        return (ysize - getY(0) - YBORDER);
+    }
+}
