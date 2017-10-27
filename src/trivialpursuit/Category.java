5bcf28c62f6451e709c2dd546c2fcc6f8e245b92
natewise <nate3wise@gmail.com>
2017-10-27 10:45:52 -0700

    added Category and Piece class

--- src/trivialpursuit/Category.java
+++ src/trivialpursuit/Category.java
@@ -4,6 +4,7 @@ package trivialpursuit;
 public class Category {
     public static final int NUM_CATEGORIES = 6;
     public static int categories[] = new int[NUM_CATEGORIES];
+    private String name;
     public static enum Categories{
         
     }
