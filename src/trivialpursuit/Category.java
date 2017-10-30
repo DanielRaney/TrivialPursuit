d4b04be05c6505af4852796b42dd839db78280d7
natewise <nate3wise@gmail.com>
2017-10-30 11:25:27 -0700

    lots

--- src/trivialpursuit/Category.java
+++ src/trivialpursuit/Category.java
@@ -5,7 +5,9 @@ import java.awt.Color;
 
 public class Category {
     public static final int NUM_CATEGORIES = 6;
+    public static final int NUM_QUESTIONS = 6;
     public static Category categories[] = new Category[NUM_CATEGORIES];
+    private Question questions[] = new Question[NUM_QUESTIONS];
     private String name;
     private Color color;
     public static enum Categories{
---
+++ src/trivialpursuit/Question.java
@@ -0,0 +1,8 @@
+
+package trivialpursuit;
+
+import static trivialpursuit.Category.NUM_QUESTIONS;
+
+public class Question {
+    private Question questions[] = new Question[NUM_QUESTIONS];  //change to answers
+}
