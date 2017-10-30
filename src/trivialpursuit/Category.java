59f805190e92165f3b7b1bb18549535ba9986202
Daniel Raney-154003389 <154003389@vusdnoc.vusd.k12.ca.us>
2017-10-30 10:35:45 -0700

    use

--- src/trivialpursuit/Category.java
+++ src/trivialpursuit/Category.java
@@ -4,6 +4,7 @@ package trivialpursuit;
 import java.awt.Color;
 
 public class Category {
+    public static int useless = 1000000000;
     public static final int NUM_CATEGORIES = 6;
     public static Category categories[] = new Category[NUM_CATEGORIES];
     private String name;
