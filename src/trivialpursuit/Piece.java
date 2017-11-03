494d056656f414eb1832f7462b4b57fe484998a7
Samuel Ortega-373000981 <373000981@vusdnoc.vusd.k12.ca.us>
2017-11-03 09:56:04 -0700

    draw Piece

--- src/trivialpursuit/Piece.java
+++ src/trivialpursuit/Piece.java
@@ -22,9 +22,19 @@ public class Piece {
         g.setColor(color); 
         g.fillOval(Window.getX(column*xdelta),Window.getY(row*ydelta),xdelta,ydelta);
         
-//        g.setColor(Color.white);
-//        g.setFont(new Font("Arial",Font.PLAIN,30));
-//        g.drawString("",Window.getX(column*xdelta)+30,Window.getY(row*ydelta)+40);           
+        int angle = (360/6);
+        g.setColor(Color.GREEN);
+        g.fillArc(Window.getX(column*xdelta)+5,  Window.getY(row*ydelta)+4, xdelta-10, ydelta-10, 90, angle-10);
+        g.setColor(Color.yellow);
+        g.fillArc(Window.getX(column*xdelta)+5,  Window.getY(row*ydelta)+4, xdelta-10, ydelta-10, 90+angle*1, angle-10);
+        g.setColor(Color.magenta);
+        g.fillArc(Window.getX(column*xdelta)+5,  Window.getY(row*ydelta)+4, xdelta-10, ydelta-10,90+angle*2, angle-10);
+        g.setColor(Color.white);
+        g.fillArc(Window.getX(column*xdelta)+5,  Window.getY(row*ydelta)+4, xdelta-10, ydelta-10, 90+angle*3, angle-10);
+        g.setColor(Color.GRAY);
+        g.fillArc(Window.getX(column*xdelta)+5,  Window.getY(row*ydelta)+4, xdelta-10, ydelta-10, 90+angle*4, angle-10);
+        g.setColor(Color.CYAN);
+        g.fillArc(Window.getX(column*xdelta)+5,  Window.getY(row*ydelta)+4, xdelta-10, ydelta-10, 90+angle*5, angle-10);        
     }
     
 }
